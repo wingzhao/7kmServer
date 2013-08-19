fa640f1d984ac2c50f761633ece41e98b42b7d7d
wingzhao <wingzhao1112@gmail.com>
2013-08-19 20:03:24 +0800

    update

---
+++ src/main/java/com/daojia/web/controller/WorkerController.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package com.daojia.web.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 服务人员Controller
+ * 
+ * @author zhaolei 2013年8月19日 下午7:14:38
+ */
+@Controller
+@RequestMapping("/worker")
+public class WorkerController extends BaseController {
+
+	/**
+	 * 测试
+	 * 
+	 * @author zhaolei 2013年8月19日 下午7:45:41
+	 * 
+	 * @return
+	 */
+	@RequestMapping(value="/test")
+	public Map<String, Object> test() {
+		Map<String, Object> relMap = new HashMap<>();
+		relMap.put("relsult", true);
+		return relMap;
+	}
+
+}
