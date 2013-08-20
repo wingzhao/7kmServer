127bde2e89ed89f33fc5f7cd03a9d85043209a67
zhaolei <zhaolei@meituan.com>
2013-08-21 01:49:22 +0800

    update

--- src/main/java/com/daojia/domain/Category.java
+++ src/main/java/com/daojia/domain/Category.java
@@ -5,13 +5,18 @@ package com.daojia.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+
 /**
  * 品类
  * 
  * @author zhaolei 2013年8月20日 下午6:24:50
  */
+@Entity
 public class Category implements Serializable{
 
 	private static final long serialVersionUID = -6428978911973623236L;
+	
+	
 
 }
