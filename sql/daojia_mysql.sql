4acf4bc7f9aeb69a2ef337bfd2ea07606bf96171
wingzhao <wingzhao1112@gmail.com>
2013-08-29 18:00:22 +0800

    update

--- sql/daojia_mysql.sql
+++ sql/daojia_mysql.sql
@@ -11,8 +11,9 @@ CREATE TABLE `category` (
 	`parent_id` INT(11) NULL DEFAULT '0' COMMENT '父品类id',
 	`cat_name` VARCHAR(20) NULL DEFAULT NULL COMMENT '品类名称',
 	`is_enable` TINYINT(1) NULL DEFAULT NULL COMMENT '是否可用 TrueOrFalse enum',
-	`lastTime` DATETIME NULL DEFAULT NULL COMMENT '最后更新时间',
-	PRIMARY KEY (`id`)
+	`last_time` DATETIME NULL DEFAULT NULL COMMENT '最后更新时间',
+	PRIMARY KEY (`id`),
+	INDEX `indx_last_time` (`last_time`)
 )
 COMMENT='品类表'
 COLLATE='utf8_general_ci'
